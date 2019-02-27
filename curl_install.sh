e7f51db204e6edf431587f068da6df03292f50ae
hellodudu86 <hellodudu86@gmail.com>
2019-02-27 11:18:47 +0800

    add curl install

--- curl_install.sh
+++ curl_install.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
 echo "start getting vimrc from github..."
-git clone git://github.com/hellodudu/vimrc_backup.git ~/.vim_runtime
+git clone --depth=1 git://github.com/hellodudu/vimrc_backup.git ~/.vim_runtime
 
 echo "start using vimrc config..."
 cd ~/.vim_runtime/
 sh ./install_awesome_vimrc.sh
 
 echo "start getting submodule plugins..."
-git submodule update --init --recursive
+git submodule update --depth=1 --init --recursive
 
 echo "start getting dependence tools..."
 
 if ! [ -x "$(command -v cmake)" ]; then
   echo "installing cmake..."
   brew install cmake
+fi
+
+if ! [ -x "$(command -v ctags)" ]; then
+  echo "installing ctags..."
+  brew install ctags
 fi
 
 ./sources_non_forked/YouCompleteMe/install.py --go-completer
